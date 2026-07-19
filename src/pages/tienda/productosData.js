b197fe38e1e0ee349bbbadb5d5612316b79eb5e2
laramonse805-tech <laramonse805@gmail.com>
2026-07-19 17:59:23 -0400

    Delete src directory

--- src/pages/tienda/productosData.js
+++
@@ -1,6 +0,0 @@
-export const productos = [
-  { id: '1', nombre: 'Polera básica', precio: 15000, emoji: '👕', descripcion: 'Polera de algodón 100%, corte unisex.' },
-  { id: '2', nombre: 'Zapatillas urbanas', precio: 32000, emoji: '👟', descripcion: 'Zapatillas cómodas para el día a día.' },
-  { id: '3', nombre: 'Mochila de viaje', precio: 25000, emoji: '🎒', descripcion: 'Mochila resistente al agua, 20L.' },
-  { id: '4', nombre: 'Gorro de lana', precio: 12000, emoji: '🧢', descripcion: 'Gorro abrigador para el invierno.' },
-]
